d5f8aa2e83c8cf56aa79f824e2793b7b4281df16
phthedevx <pedro.h.santos20@ba.estudante.senai.br>
2025-12-31 21:33:15 -0300

    chore: update domain url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   // 🔴 Mude a URL abaixo para a sua (quando tiver o deploy, ex: https://pedrohenrique.vercel.app)
   // Se não tiver URL ainda, pode deixar localhost ou uma string vazia por enquanto
-  siteUrl: process.env.SITE_URL || 'https://seu-portfolio.vercel.app', 
+  siteUrl: process.env.SITE_URL || 'https://www.phthedev-vercel.app',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
 }
